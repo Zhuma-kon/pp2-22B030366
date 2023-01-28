a8662604ffe857b2f5d92c66fc77674d7e6b47de
Zhuma-kon <i_zhumadildinov@kbtu.kz>
2023-01-29 02:13:40 +0600

    tsis1 is half way done

---
+++ tsis1/tsis1-8.py
@@ -0,0 +1,4 @@
+x=int(3)
+y=float(4.5)
+z=complex(5)
+w=str(4)
---
+++ tsis1/tsis1-9.py
@@ -0,0 +1,7 @@
+print("Hello")
+print('Hello')
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
