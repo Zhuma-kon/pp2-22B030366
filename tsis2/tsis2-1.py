249e776bd4212761e1dd4f9018da1deb77b45234
Zhuma-kon <i_zhumadildinov@kbtu.kz>
2023-01-31 01:58:11 +0600

    tsis2.1

--- tsis2/tsis2-1.py
+++ tsis2/tsis2-1.py
@@ -22,9 +22,9 @@ while i < 6:
   print(i)
 
 
-  i = 1
-while i < 6:
-  print(i)
-  i += 1
-else: # else is still else
+y = 1
+while y < 6:
+  print(y)
+  y += 1
+else:         # else is still else
   print("i is no longer less than 6")
